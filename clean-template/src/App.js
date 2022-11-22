c77283ae34380024068deb32b7c1260850a7da39
armiche <armiramos93@gmail.com>
2022-11-22 18:46:26 +0000

    clean-template

---
+++ clean-template/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+        molestias obcaecati hic iure quis libero quam rerum repellendus
+        deserunt, aut, porro harum! Consequatur similique itaque porro provident
+        odio exercitationem quod illo repudiandae dolorum, facilis temporibus in
+        accusantium rerum, labore nemo culpa fuga eligendi doloribus, nisi cum
+        ipsam rem modi necessitatibus?
+      </p>
+    </div>
+  );
+}
+
+export default App;
